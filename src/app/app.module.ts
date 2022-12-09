e89e88a5a14581a54597a98aa152fdaecf2acdf9
gd <gd-git@s.gdoeppert.de>
2022-12-09 23:03:25 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
@@ -18,6 +19,7 @@ import { NxGridModule } from '@aposin/ng-aquila/grid';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule, ReactiveFormsModule,
     HttpClientModule,
     NxExpertModule,
