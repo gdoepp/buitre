587e7ac900f99b4acbf539deeb3183cca023b3ce
gd <gd-git@gdoeppert.de>
2025-07-19 22:03:51 +0200

    patched ng-aquila

--- karma.conf.js
+++ karma.conf.js
@@ -11,8 +11,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-electron'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage')
+    //,  require('@angular/build/builders/karma')
     ],
     client: {
       jasmine: {
